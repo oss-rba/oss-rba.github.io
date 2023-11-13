f2053ba6c165d4a320a97d9bb24816c4eabbe1d6
oss-rba <ossdev80@gmail.com>
2023-11-13 15:23:28 +0700

    fetch nib statuspenmamanamodal

--- src/endpoint.js
+++ src/endpoint.js
@@ -3176,7 +3176,8 @@
  * Oss Id adalah ID yang di Generate Sistem OSS dan Dikirimkan ke K/L/D Bersama Permohonan Nomor Induk Berusaha
  * @apiSuccess (200) {string(2){2}} responfetchNIB.dataNIB.jenis_pelaku_usaha Jenis Pelaku Usaha (* Lihat Lampiran 18)
  * @apiSuccess (200) {string(2){2}} responfetchNIB.dataNIB.jenis_perusahaan Jenis Perusahaan (* Lihat Lampiran 2)
- * @apiBody {string(10){10}} dataNIB.skala_usaha Skala Usaha Perusahaan
+ * @apiSuccess {string(10){10}} responfetchNIB.dataNIB.skala_usaha Skala Usaha Perusahaan
+ * @apiSuccess (200) {string(2)} responfetchNIB.dataNIB.status_penanaman_modal	Status Penanaman Modal (* Lihat Lampiran 1)		
  * @apiSuccess (200) {string(255){255}} responfetchNIB.dataNIB.nama_perseroan Nama Perseroan
  * @apiSuccess (200) {string(255){255}} responfetchNIB.dataNIB.alamat_perseroan Alamat Perseroan
  * @apiSuccess (200) {string(7){7}} responfetchNIB.dataNIB.rt_rw_perseroan	Rt Rw Perseroan
@@ -3217,6 +3218,7 @@
  *            "jenis_pelaku_usaha": null,
  *            "jenis_perusahaan": "",
  *            "skala_usaha": "",
+ *            "status_penanaman_modal": "",
  *            "nama_perseroan": "",
  *            "alamat_perseroan": "",
  *            "rt_rw_perseroan": "",
